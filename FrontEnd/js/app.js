3bf54dadce4c53628936fac785ba26701f9d4836
Quack Ranger <vaden130@gmail.com>
2025-09-24 13:17:20 +0200

    Ajout login

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -1,8 +1,13 @@
 const gallery = document.querySelector(".gallery");
 const filtres = document.querySelector("#filtres");
+const pills = document.querySelector(".pills");
+const all = document.querySelector("#all");
 
 const urlWorks = "http://localhost:5678/api/works/";
 const urlCat = "http://localhost:5678/api/categories/";
+
+let works = [];
+let categories = [];
 
 Promise.all([
   fetch(urlWorks).then((res) => {
@@ -18,32 +23,67 @@ Promise.all([
     return res.json();
   }),
 ])
-  .then(([works, categories]) => {
-    works.map(function (work) {
-      const fig = document.createElement("figure");
-      const img = document.createElement("img");
-      const figCaption = document.createElement("figcaption");
+  .then(([worksData, categoriesData]) => {
+    works = worksData;
+    categories = categoriesData;
 
-      img.src = `${work.imageUrl}`;
-      img.alt = `${work.title}`;
-      figCaption.innerHTML = `${work.title}`;
+    showWorks(works);
 
-      fig.appendChild(img);
-      fig.appendChild(figCaption);
-      gallery.appendChild(fig);
-    });
-
-    categories.map(function (categorie) {
-      const pills = document.createElement("ul");
+    categories.map((category) => {
       const pill = document.createElement("li");
-
-      pill.innerHTML = `${categorie.name}`;
+      pill.innerHTML = `${category.name}`;
       pill.classList.add("pill");
-
       pills.appendChild(pill);
-      filtres.appendChild(pills);
+
+      pill.addEventListener("click", () => {
+        pills.querySelectorAll("li").forEach((pill) => {
+          if (pill.classList.contains("selected")) {
+            pill.classList.remove("selected");
+          }
+        });
+
+        pill.classList.toggle("selected");
+        filterByCategory(category.name);
+      });
+
+      if (all) {
+        all.addEventListener("click", () => {
+          pills.querySelectorAll("li").forEach((pill) => {
+            if (pill.classList.contains("selected")) {
+              pill.classList.remove("selected");
+            }
+          });
+          all.classList.add("selected");
+          showWorks(works);
+        });
+      }
     });
   })
   .catch((error) => {
     console.error("Erreur:", error);
   });
+
+function showWorks(worksToDisplay) {
+  gallery.innerHTML = "";
+
+  worksToDisplay.map((work) => {
+    const fig = document.createElement("figure");
+    const img = document.createElement("img");
+    const figCaption = document.createElement("figcaption");
+
+    img.src = `${work.imageUrl}`;
+    img.alt = `${work.title}`;
+    figCaption.innerHTML = `${work.title}`;
+
+    fig.appendChild(img);
+    fig.appendChild(figCaption);
+    gallery.appendChild(fig);
+  });
+}
+
+function filterByCategory(nomCategorie) {
+  const worksFiltres = works.filter(
+    (work) => work.category.name === nomCategorie
+  );
+  showWorks(worksFiltres);
+}
